560354734e61892989eefbe181929bb9b9a6d48f
y41000 <yhbia1000@gmail.com>
2024-05-13 00:26:32 -0400

    First basic flux.

--- api/instructor.py
+++ api/instructor.py
@@ -1,42 +1,62 @@
 from openai import OpenAI
+
+import matplotlib
+matplotlib.use('TkAgg')  # Usar la backend TkAgg, que es interactiva
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import time
 import base64
 import os
 # import requests
 
+# Path to the image
+image_path = "./images/"
+user_poker_name="pokeados"
+
 api_key = os.getenv('POKER_GPT_KEY')
 if api_key is None:
-    raise ValueError("API_KEY no encontrada. Asegúrate de configurar esta variable de entorno.")
+    raise ValueError("API_KEY n0t found. Save your API key in a environment variable!")
+
+client = OpenAI(api_key=api_key)
 
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to the image
-image_path = "images/mano.png"
-
-# Getting the base64 string
-base64_image = encode_image(image_path)
 
 # GPT-4 magic
-pedido="Dame información que puedas extraer para yo realizar mi apuesta teniendo en cuenta que UTG raiseó u los dos siguientes foldearon. Te hago las siguientes preguntas enumeradas: 1.¿Puedes diferenciar el monto que apostó UTG? 2.¿Qué manos podrían ser más fuertes que la mía? 3.¿Qué debo hacer si a priori no conozco nada de ningún jugador?"
-client = OpenAI(api_key=api_key)
 
-response = client.chat.completions.create(
-    model="gpt-4-turbo",
-    messages=[{"role": "user",
-               "content": [
-                   {
-                       "type": "text", "text": pedido
-                    },
-                   {
-                       "type": "image_url",
-                       "image_url": {
-                           "url": f"data:image/jpeg;base64,{base64_image}"
-                       }
-                    }
-                   ],
-               "name":"Ludio"}]
-)
-print(f"PREGUNTA: {pedido}\n")
-print(f"RESPUESTA: {response.choices[0].message.content}")
+pedido=f"You are a profesio0nal poker player called {user_poker_name}. Tell me 2 things: 1º what else do you need to make a move. 2º What move would you do."
+
+for im in os.listdir(image_path):
+    if im.split(".")[-1]=="png":
+        # Getting the base64 string
+        base64_image = encode_image(f"{image_path}{im}")
+
+        response = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[{"role": "user",
+                    "content": [
+                        {
+                            "type": "text", "text": pedido
+                            },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                            }
+                        ]
+                        }]
+        )
+        
+        # Instructors answer
+        print(f"RESPUESTA: {response.choices[0].message.content}")
+        # Show the image
+        imag = mpimg.imread(f"{image_path}{im}")
+        plt.imshow(imag)
+        plt.axis('off')  # Ocultar los ejes
+        plt.show()
+        #time.sleep(5)
+        #plt.close()
